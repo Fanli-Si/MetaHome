929b8baa02edaf567b5d01e03eab8a432ec0e52f
Kadyn Neale <Kneale95@hotmail.ca>
2022-04-15 11:47:22 -0700

    Revert "added find item by id"
    This reverts commit 345fd06febdeec4ee42ee6127093401edc384c66.

--- src/main/java/com/csis3275/demo/controller/ItemController.java
+++ src/main/java/com/csis3275/demo/controller/ItemController.java
@@ -2,21 +2,18 @@ package com.csis3275.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csis3275.demo.model.Item;
 import com.csis3275.demo.model.ItemRepository;
-import com.csis3275.demo.model.Order;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
@@ -44,17 +41,5 @@ public class ItemController {
 		} catch (Exception e) {
 			return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-	}
-	
-	@GetMapping("/items/{itemId}")
-	public ResponseEntity<Item> getItemByName(@PathVariable("itemId") Long itemId) {
-		Optional<Item> itemData = itemRepository.findById(itemId);
-
-		if (itemData.isPresent()) {
-			return new ResponseEntity<>(itemData.get(), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-
 	}
 }
